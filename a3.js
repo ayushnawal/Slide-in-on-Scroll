3cb16eaea84b7742442d2a271f6e77c1f83fcbe1
Ayush Nawal <36100944+ayushnawal@users.noreply.github.com>
2018-08-10 23:40:24 +0530

    Add files via upload

--- a3.js
+++ a3.js
@@ -30,3 +30,23 @@ function checkSlide(){
   });
 }
 window.addEventListener('scroll', debounce(checkSlide));
+const triggers = document.querySelectorAll('p');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
+
+function highlightLink(){
+   const linkCoords = this.getBoundingClientRect();
+   console.log(linkCoords);
+   const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+   };
+
+   highlight.style.width = `${coords.width}px`;
+   highlight.style.height = `${coords.height}px`;
+   highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+triggers.forEach(p => p.addEventListener('mouseenter',highlightLink));
