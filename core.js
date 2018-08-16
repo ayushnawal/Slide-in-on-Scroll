5ed5610829f22bb2bba95bca1fb4d6bbd04621a4
Ayush Nawal <36100944+ayushnawal@users.noreply.github.com>
2018-08-17 03:31:57 +0530

    Add files via upload

--- core.js
+++ core.js
@@ -50,3 +50,18 @@ function highlightLink(){
    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 triggers.forEach(p => p.addEventListener('mouseenter',highlightLink));
+
+const nav = document.querySelector('#main');
+let topOfNav = nav.offsetTop;
+
+function fixNav(){
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  }else{
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop=0;
+  }
+}
+
+window.addEventListener('scroll', fixNav);
